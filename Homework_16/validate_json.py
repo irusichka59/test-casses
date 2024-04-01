9edf2cfb3c6f34742dc307568dc01d821253ca01
irusichka59 <irusichka59@gmail.com>
2024-04-01 22:14:19 +0300

    TEST_Cases

--- Homework_16/validate_json.py
+++ Homework_16/validate_json.py
@@ -4,20 +4,21 @@ from Homework_16.Schema.pets_schema import PetsSchema
 from marshmallow import ValidationError
 
 
-def validate_json(json_file):
+def validate_json_from_file(json_file):
     # Завантаження JSON-даних з файлу
     with open(json_file, 'r') as file:
         data = json.load(file)
 
     # Виконання валідації JSON-даних
     try:
-        result = PetsSchema(many=True).load(data)
+        pets = PetsSchema(many=True).load(data)
         print("Дані є відповідними схемі.")
-        return result
+        for pet in pets:
+            tags = [tag['name'] for tag in pet['tags']]
+            print(pet['name'], pet['category']['name'], tags)
     except ValidationError as e:
         print("Помилка валідації:", e)
 
 
 if __name__ == '__main__':
-    validate_json('my_json_file.json')
-
+    validate_json_from_file('my_json_file.json')
